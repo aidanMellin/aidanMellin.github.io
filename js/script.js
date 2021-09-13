c0ffcc6dfdfa77d9b6d7becfcfaace09c5ea7983
aidanMellin <aidan.mellin@gmail.com>
2021-09-13 17:36:33 -0400

    added a projects page. This was complicated lmao

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 const container = document.querySelector('.container');
 const toBlogButton = document.querySelector('.blogBtn');
 const toAboutButton = document.querySelector('.aboutBtn');
+const toProjButton = document.querySelector('.projBtn')
 const toHomeBtns = document.querySelectorAll('.toHome');
 
 function toHome(){
-  container.classList.remove('toBlog','toAbout');
+  container.classList.remove('toProj','toBlog','toAbout');
+  console.log("toHome")
+}
+
+function loadHome(){
+  window.location.href = 'index.html#splashHome' 
 }
 
 toBlogButton.addEventListener('click',()=>{
@@ -13,6 +19,10 @@ toBlogButton.addEventListener('click',()=>{
 
 toAboutButton.addEventListener('click',()=>{
   container.classList.toggle('toAbout');
+})
+
+toProjButton.addEventListener('click',()=>{
+  container.classList.toggle('toProj');
 })
 
 toHomeBtns.forEach(element=>{
